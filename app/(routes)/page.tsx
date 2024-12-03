9fa3ac70926b8421ef3643851a092eb8a724f22e
alfredo dumaine <alfredo.dumaine@ine.mx>
2024-12-02 18:33:36 -0600

    Creacion de proyecto

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { CardSummary } from "./components/CardSumary";
+import { BookOpenCheck, Icon, UsersRound, Waypoints } from "lucide-react";
+
+const dataCardsSummary = [
+  {
+    Icon:UsersRound,
+    total:"12.450",
+    average:15,
+    title:"Companies created",
+    tooltipText:"See all of the companies created" 
+  },
+  {
+    Icon:Waypoints,
+    total:"86.5%",
+    average:80,
+    title:"Total Revenue",
+    tooltipText:"See all of the summary"
+  },
+  {
+    Icon:BookOpenCheck,
+    total:"363,95",
+    average:30,
+    title:"Bounce Rate",
+    tooltipText:"See all of the bounce rate"
+  },
+]
+
+export default function Home() {
+  return (
+   <div>
+      <h1 className="text-2xl mb-4">dashboard</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">   
+        {dataCardsSummary.map(({Icon, total, average, title, tooltipText}) => (
+         <CardSummary
+          key={title}
+          icon={Icon}
+          total={total}
+            average={average}
+            title={title}
+            tooltipText={tooltipText}
+          />
+        ))}
+              
+      </div>
+   </div> 
+  );
+}
